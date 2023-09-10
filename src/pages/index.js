6ec76e5e23ff5963cb4c278d2f3bff01691b3e41
shouryade <shourya.de12@gmail.com>
2023-09-10 14:47:35 +0530

    code zone :sparkles:
    Signed-off-by: shouryade <shourya.de12@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -114,7 +114,7 @@ const IndexPage = () => {
         <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
           <div class="mr-auto place-self-center lg:col-span-7">
             <h1 class="max-w-2xl mb-4 text-4xl font-bold tracking-tight leading-none md:text-5xl xl:text-6xl !text-white">
-              Get a Job faster
+              Code Zone
             </h1>
             <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl !text-gray-400">
               At Code Zone, we're not just an online developer training
